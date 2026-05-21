10a14fcac9a8d18d007560b48b3af5227eb12feb
Vishnu <vishnupraks@gmail.com>
2026-05-21 15:55:04 -0700

    fix: redirect to index.php upon successful login instead of loggedin.php

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION["user"] = $user["username"];
         $_SESSION["role"] = $user["role"];
 
-        header("Location: loggedin.php");
+        header("Location: index.php");
         exit;
 
     } else {
